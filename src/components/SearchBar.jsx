f03bfb940278d9f4353874659587c5c62dc58b29
ABDULHAKIM <abdulhakimsudi27@gmail.com>
2025-04-13 16:40:42 +0300

    Implement basic structure for SearchBar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+// src/components/SearchBar.jsx
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <input type="text" placeholder="Search expenses..." />
+  );
+};
+
+export default SearchBar;
